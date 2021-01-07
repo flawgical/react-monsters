e12c6c649472486f16d10eaad146f23f6001aae3
Shahzad Khan <flawgical@ShahzadComputer.attlocal.net>
2021-01-06 22:23:31 -0600

    adds basic css in components and creates user card component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { CardList } from './components/car-list/card-list.component'
 import './App.css';
 
 class App extends Component {
@@ -16,10 +17,13 @@ class App extends Component {
   render(){
       return (
         <div className="App">
+          <CardList name='Shazy'>
           {
             this.state.monsters.map(monster => 
             <h1 key={monster.id}> { monster.name }</h1> )
           }
+          </CardList>
+         
         </div>
       );
   }
